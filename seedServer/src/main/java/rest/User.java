227194b575a50e430545bb420a3376cb7bc38e95
Adam Becvar <becvar.adam666@gmail.com>
2017-10-31 10:10:16 +0100

    Adding rest endpoint for editing user

--- seedServer/src/main/java/rest/User.java
+++ seedServer/src/main/java/rest/User.java
@@ -1,35 +1,34 @@
 package rest;
 
 import com.google.gson.Gson;
-import facades.RandomNumber;
-import javax.annotation.security.RolesAllowed;
-import javax.ws.rs.GET;
+import facades.UserFacade;
+import security.IUserFacade;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("demouser")
-@RolesAllowed("User")
+@Path("user")
 public class User {
     Gson gson;
-    public User(){
-        gson = new Gson();
+    IUserFacade facade;
+
+    public User() {
+        this.gson = new Gson();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu_development");
+        facade = new UserFacade(emf);
     }
 
-    @GET
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String getSomething() {
-        return "{\"message\" : \"Hello User from Server (Accesible by only authenticated USERS)\"}";
-    }
-
-    @GET
-    @Path("random")
-    @Produces(MediaType.APPLICATION_JSON)
-    public String getNumber() {
-        int number;
-        RandomNumber Rnd = new RandomNumber();
-        number = Rnd.getRndNumber();
-
-        return gson.toJson(number);
+    public String editUser(String json) {
+        entity.User user = gson.fromJson(json, entity.User.class);
+        facade.editUser(user);
+        return gson.toJson(user);
     }
 }
