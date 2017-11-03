b7d953e25e97654a123433b925b056c264e5b0b0
petcat <catanapetru@gmail.com>
2017-11-03 09:51:32 +0100

    added filter

--- seedClient/src/pages/Places.js
+++ seedClient/src/pages/Places.js
@@ -1,37 +1,60 @@
 import React from 'react';
-
+import { serverURL } from '../config.json'
 export default class Places extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            places: []
+            places: [],
+            initialPLaces: []
         }
     }
     componentDidMount() {
-        fetch("http://b4c478ab.ngrok.io/api/places")
+        fetch(serverURL + "api/places")
             .then(res => {
                 return res.json();
             })
             .then(places => {
-                this.setState({ places })
+                this.setState({ places, initialPLaces: places })
                 console.log("places", places)
             })
     }
+    
+    filterList = (event) => {
+        var updatedList = this.state.initialPLaces;
+        var value = event.target.value.toLowerCase();
+        updatedList = updatedList.filter(function(place)  {
+            return place.city.toLowerCase().search(value) !== -1 ||
+                   place.description.toLowerCase().search(value) !== -1           
+        });
+        this.setState({places: updatedList});
+    }
+
 
     render() {
         const places = this.state.places;
-        console.log("render places", places);
+        
         return (
             <div className="container">
-                <div className="row">
+                <h2>Places</h2>
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        onChange={this.filterList}
+                    />
+                </div>
+                {console.log("query", this.state.query)}
+                <div id="list" className="row">
                     {
                         places.map((place) => {
                             return (
                                 <div key={place.id} className="col-sm-2" style={{ width: 254 }}>
-                                    <img  src={place.imageUrl} style={{ width: 250 }} />
+                                    <img src={place.imageUrl} style={{ width: 250 }} />
                                     <div >
                                         <h4 >{place.city}</h4>
-                                        <p >{place.description}</p>
+                                        <p>{place.description}</p>
+                                        <p>ZIP: {place.zip}</p>
+                                        <p>Rating: {place.rating}</p>
                                     </div>
                                 </div>
                             )
--- seedServer/src/main/java/deploy/DeploymentConfiguration.java
+++ seedServer/src/main/java/deploy/DeploymentConfiguration.java
@@ -21,80 +21,100 @@ import java.util.logging.Logger;
 @WebListener
 public class DeploymentConfiguration implements ServletContextListener {
 
-  public static String PU_NAME = "CA3";
+    public static String PU_NAME = "CA3";
 
-  @Override
-  @SuppressWarnings("empty-statement")
-  public void contextInitialized(ServletContextEvent sce) {
-    System.out.println("######################################################################################");
-    System.out.println("############################ In ContextIntialized ####################################");
-    System.out.println("######################################################################################");
+    @Override
+    @SuppressWarnings("empty-statement")
+    public void contextInitialized(ServletContextEvent sce) {
+        System.out.println("######################################################################################");
+        System.out.println("############################ In ContextIntialized ####################################");
+        System.out.println("######################################################################################");
 
-    //Handling init-params from the properties file (secrets that should not be pushed to GIT)
-    InputStream input = null;
-    Properties prop = new Properties();
-    try {
-      input = getClass().getClassLoader().getResourceAsStream("/config.properties");;
-      if (input == null) {
-        System.out.println("Could not load init-properties");
-        return;
-      }
-      prop.load(input);
-      Secret.SHARED_SECRET = prop.getProperty("tokenSecret").getBytes();
-      input.close();
+        //Handling init-params from the properties file (secrets that should not be pushed to GIT)
+        InputStream input = null;
+        Properties prop = new Properties();
+        try {
+            input = getClass().getClassLoader().getResourceAsStream("/config.properties");;
+            if (input == null) {
+                System.out.println("Could not load init-properties");
+                return;
+            }
+            prop.load(input);
+            Secret.SHARED_SECRET = prop.getProperty("tokenSecret").getBytes();
+            input.close();
 
-    } catch (IOException ex) {
-      Logger.getLogger(DeploymentConfiguration.class.getName()).log(Level.SEVERE, null, ex);
-    }
-    ServletContext context = sce.getServletContext();
- 
-    boolean makeTestUser = context.getInitParameter("makeTestUser").toLowerCase().equals("true");
-    if (makeTestUser) {
-      EntityManager em = Persistence.createEntityManagerFactory(PU_NAME).createEntityManager();
-      try {
-        System.out.println("Creating TEST Users");
-        if (em.find(User.class, "user") == null) {
-          em.getTransaction().begin();
-          Role userRole = new Role("User");
-          Role adminRole = new Role("Admin");
-          User user = new User("user", "test");
-          user.addRole(userRole);
-          User admin = new User("admin", "test");
-          admin.addRole(adminRole);
-          User both = new User("user_admin", "test");
-          both.addRole(userRole);
-          both.addRole(adminRole);
-          em.persist(userRole);
-          em.persist(adminRole);
-          em.persist(user);
-          em.persist(admin);
-          em.persist(both);
-          em.getTransaction().commit();
-          System.out.println("Created TEST Users");
+        } catch (IOException ex) {
+            Logger.getLogger(DeploymentConfiguration.class.getName()).log(Level.SEVERE, null, ex);
         }
-        if(em.find(Place.class, "place") == null){
-            em.getTransaction().begin();
-            Place place = new Place();
-            place.setAddress("Wall Street, 26");
-            place.setCity("New York");
-            place.setDescription("Where the money at!");
-            place.setRating(5);
-            place.setZip("6666");
-            place.setImageUrl("http://financeblvd.com/wp-content/uploads/2017/08/wallstreetfeature.jpg");  
-            em.getTransaction().commit();
-            System.out.println("Created Places");
+        ServletContext context = sce.getServletContext();
+
+        boolean makeTestUser = context.getInitParameter("makeTestUser").toLowerCase().equals("true");
+        if (makeTestUser) {
+            EntityManager em = Persistence.createEntityManagerFactory(PU_NAME).createEntityManager();
+            try {
+                System.out.println("Creating TEST Users");
+                if (em.find(User.class, "user") == null ) {
+                    em.getTransaction().begin();
+                    Role userRole = new Role("User");
+                    Role adminRole = new Role("Admin");
+                    User user = new User("user", "test");
+                    user.addRole(userRole);
+                    User admin = new User("admin", "test");
+                    admin.addRole(adminRole);
+                    User both = new User("user_admin", "test");
+                    both.addRole(userRole);
+                    both.addRole(adminRole);
+                    em.persist(userRole);
+                    em.persist(adminRole);
+                    em.persist(user);
+                    em.persist(admin);
+                    em.persist(both);
+
+                    System.out.println("Created TEST Users");
+
+                    Place place = new Place();
+                    place.setAddress("Wall Street, 26");
+                    place.setCity("New York");
+                    place.setDescription("Where the money at!");
+                    place.setRating(5);
+                    place.setZip("6666");
+                    place.setImageUrl("http://financeblvd.com/wp-content/uploads/2017/08/wallstreetfeature.jpg");
+                    
+                    Place place2 = new Place();
+                    place2.setAddress("Nuhavn 7");
+                    place2.setCity("Kobenhavn");
+                    place2.setDescription("WHere the blondes at");
+                    place2.setImageUrl("http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/header-facebook-copenhagen-cph0416.jpg?itok=-2LcgY1H");
+                    place2.setZip("1234");
+                    place2.setRating(4);
+                    
+                    Place place3 = new Place();
+                    place3.setAddress("Praca do Comercio");
+                    place3.setCity("Lisbon");
+                    place3.setDescription("Where the sun is");
+                    place3.setImageUrl("https://media.cntraveler.com/photos/58f984542867946a9cbe1f11/master/pass/praca-do-comercio-lisbon-GettyImages-648812458.jpg");
+                    place3.setZip("3000");
+                    place3.setRating(5);
+                    
+                    em.persist(place);
+                    em.persist(place2);
+                    em.persist(place3);
+                    System.out.println("Created Places");
+                    
+                    em.getTransaction().commit();
+                }
+
+            } catch (Exception ex) {
+                Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
+                em.getTransaction().rollback();
+            } finally {
+                em.close();
+            }
         }
-      } catch (Exception ex) {
-        Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-        em.getTransaction().rollback();
-      } finally {
-        em.close();
-      }
     }
-  }
 
-  @Override
-  public void contextDestroyed(ServletContextEvent sce) {
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
 
-  }
+    }
 }
